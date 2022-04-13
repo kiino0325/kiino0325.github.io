8f764b53cc947ddb67395a888c0cbd6bba2f4309
kiino0325 <55965012+kiino0325@users.noreply.github.com>
2022-04-12 23:14:23 -0400

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$('#Question').on('input', function () {
+  $(wages).hide()
+})
+$('#Comment').on('input', function () {
+  $(wages).hide()
+})
+$('#Hiring').on('input', function () {
+  $(wages).show()
+})
+
